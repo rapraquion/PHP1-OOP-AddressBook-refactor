739314a6842e54a963c75f0e327bfa76cd16da90
rapraquion <rapraquion@gmail.com>
2020-03-19 17:18:31 +0800

    fixing CRUD functionalities

--- addessbook/Objects/Connection.php
+++ addessbook/Objects/Connection.php
@@ -1,17 +1,39 @@
 <?php 
 
 //Include Interface Iconnect here..
+include 'IConnect.php';
 
 Class Connection implements IConnect {
 
 	//Variables
+	const HOST = "localhost";
+	const USER = "root";
+	const PWD = "";
+	const DBNAME = "addressbook";
 
-	public function __construct()
-	{
-
+	protected function __construct() {
+		self::HOST;
+		self::USER;
+		self::PWD;
+		self::DBNAME;
 	}
 
 	//Add your methods below
+	public function connect() {
+		$dsn = "mysql:host=".self::HOST.";dbname=".self::DBNAME.";charset=utf8mb4";
+		$options = [
+			PDO::ATTR_EMULATE_PREPARES   => false,
+			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		];
+		try {
+			$pdo = new PDO($dsn, self::USER, self::PWD, $options);
+			echo "Connected";
+		} catch (Exception $e) {
+			error_log($e->getMessage());
+			exit('Something weird happened');
+		}
+	}
 }
 
 ?>
--- addessbook/Objects/Query.php
+++ addessbook/Objects/Query.php
@@ -1,32 +1,52 @@
 <?php 
 
 //Include Connection class here..
+include 'Connection.php';
 
 Class Query extends Connection {
 	
 	public function __construct()
 	{
-	   parent::_construct();
+	   parent::connect();
 	}
 
 	public function display()
 	{
 	  //your select code here
+	  $sql = "SELECT * FROM address WHERE name = ?";
+	 
 	}
 
 	public function insert($get)
 	{
 	  //your insert code here
+	//   $sql = "INSERT INTO address(name, phone, email) VALUES (?,?,?)";
+	//   $stmt = parent::connect()->prepare($sql);
+	//     if($stmt) {
+	//   		$stmt->execute([$get[0], $get[1], $get[2]]);
+	// 	} else {
+	// 		echo "Database Error!";
+	// 	}
 	}
 
 	public function update($id,$get)
 	{
 	 //your update code here
+	//  $sql = "UPDATE address SET name=?,phone=?,email=? WHERE id=?";
+	//  $stmt = parent::connect()->prepare($sql);
+	// 	if($stmt) {
+	//  		$stmt->execute([$get[0], $get[1], $get[2], $id]);
+	// 	} else {
+	// 		echo "Database Error!";
+	// 	}
 	}
 
 	public function delete($id)
 	{
 	 //your delete code
+	//  $sql = "DELETE FROM address WHERE id = ?";
+	//  $stmt = parent::connect()->prepare($sql);
+	//  $stmt->execute([$id]);
 	}
 } 
 
